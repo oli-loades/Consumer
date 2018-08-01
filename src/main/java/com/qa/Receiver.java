0d763b409e71ce9a3eaa3a20f6ff9bb9b3e93cff
oli <oli.loades@gmail.com>
2018-08-01 12:07:17 +0100

    added fuse call

--- src/main/java/com/qa/Receiver.java
+++ src/main/java/com/qa/Receiver.java
@@ -1,11 +1,19 @@
 package com.qa;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class Receiver {
 
 	public void receiveMessage(String message) {
 		System.out.println("Received <" + message + ">");
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		String result = restTemplate.getForObject("http://localhost:9000/employeeservice/employees/{employee}",
+				String.class, message);
+		
+		System.out.println(result);
 	}
 }
